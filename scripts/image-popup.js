13e2a9fd8f35accebfbe94601a596b52c6755351
jkmkowalczyk <jkm.kowalczyk@gmail.com>
2019-02-10 13:42:28 +0100

    added no scroll when popup is shown

--- scripts/image-popup.js
+++ scripts/image-popup.js
@@ -6,13 +6,45 @@ let popupImage = document.getElementById("popup-image");
 let closeButton = document.getElementById("close-button");
 
 function showPopup() {
+    disableScroll();
     popup.style.display = "flex";
     popupImage.src = this.src;
 }
 
 function closePopup() {
+    enableScroll();
     popup.style.display = "none";
 }
+
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    let keys = {37: 1, 38: 1, 39: 1, 40: 1};
+    if (keys[e.key]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+    window.onwheel = preventDefault;
+    window.onmousewheel = document.onmousewheel = preventDefault;
+    window.ontouchmove = preventDefault;
+    document.onkeydown = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
+
 
 bobber.addEventListener("click", showPopup);
 scrambler.addEventListener("click", showPopup);
